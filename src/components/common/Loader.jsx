3dbb08bc2bdf526e7eae835293850e48c9ed00a0
miracool <makurseme@gmail.com>
2022-09-01 17:08:11 +0100

    feature: completed the boilerplate

---
+++ src/components/common/Loader.jsx
@@ -0,0 +1,9 @@
+import React from ' className="bg-black text-blue-800"react'
+
+const Loader = () => {
+  return (
+    <div className="bg-black text-blue-800">Loader</div>
+  )
+}
+
+export default Loader
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,41 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '450px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'text-pry': '#AD7D00',
+      'text-sec': '#282828',
+      'text-base': '#eee',
+      'main-text': '#AEAEAE',
+    },
+    backgroundColor:{
+      'bg-pry': '#AEAEAE',
+      'bg-dark': '#282828',
+      'bg-orange': '#AD7D00',
+    },
+    fontFamily: {
+      SpaceGrotesk: ['SpaceGrotesk', 'sans-serif'],
+      SpaceGroteskLight:['SpaceGrotesk-Light' , 'sans-serif'],
+      SpaceGroteskBold: ['SpaceGrotesk-Bold' , 'sans-serif'],
+    },
+    extend: {
+      spacing: {
+        'sm': '3px',
+        'md': '6px',
+        'lg': '9px',
+        'xl': '12px',
+      },
+      borderRadius: {
+        'sm': '32px',
+        '3xl': '42px',
+        'round': '50%'
+      },
+    },
   },
   plugins: [],
 }
